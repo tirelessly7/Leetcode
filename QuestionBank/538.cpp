a3d08eebc4f3ea10325af8e840f11c93cf0f9834
tirelessly7 <71633147+tirelessly7@users.noreply.github.com>
2020-09-26 07:15:36 +0000

    Create 538.cpp

---
+++ QuestionBank/538.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int sum = 0;
+
+    TreeNode* convertBST(TreeNode* root) {
+        if (root != nullptr)
+        {
+            convertBST(root->right);
+            sum += root->val;
+            root->val = sum;
+            convertBST(root->left);
+        }
+        return root;
+    }
+};
